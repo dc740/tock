4023434be6dc0f42b2a0a12ef15f8fee7191ea01
Emilio Moretti <emilio.moretti@gmail.com>
2018-09-19 01:18:45 -0300

    Finally found the cause.
    I totally forgot about main.rs, and it was trying to include std there.
    Now it compiles (it doesn't mean it works)

--- boards/ciaa/edu-ciaa/src/main.rs
+++ boards/ciaa/edu-ciaa/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![feature(panic_implementation)]
+//#![deny(missing_docs)]
+
+extern crate capsules;
+#[allow(unused_imports)]
+#[macro_use(create_capability, debug, debug_gpio, static_init)]
+extern crate kernel;
+extern crate cortexm4;
+
+use core::panic::PanicInfo;
+#[cfg(not(test))]
+#[panic_implementation]
+#[no_mangle]
+pub unsafe extern "C" fn panic_fmt(pi: &PanicInfo) -> ! {
+	loop { }
 }
