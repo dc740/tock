654bcfee6121c40336d17d418f0fcbd7229b4b95
Emilio Moretti <emilio.moretti@gmail.com>
2020-05-29 07:10:03 -0300

    Finally, we have a debug interface again

--- boards/ciaa/edu-ciaa/gdb_session.sh
+++ boards/ciaa/edu-ciaa/gdb_session.sh
@@ -5,11 +5,11 @@ trap "kill 0" EXIT
 # we need to be in the root folder or it won't find the sources.
 cd ../../../
 ### OpenOCD
-openocd -c "gdb_port 3333" -c "telnet_port 4444" -c "tcl_port 6666" -f ftdi_lpc4337.cfg >> /dev/null 2>&1 &
-#arm-none-eabi-gdb --tui target/thumbv7em-none-eabi/debug/edu-ciaa -x gdb_from_commandline_startup_commands
+openocd -c "gdb_port 3333" -c "telnet_port 4444" -c "tcl_port 6666" -f boards/ciaa/edu-ciaa/ftdi_lpc4337.cfg >> /dev/null 2>&1 &
+#arm-none-eabi-gdb --tui target/thumbv7em-none-eabi/debug/edu-ciaa -x boards/ciaa/edu-ciaa/gdb_from_commandline_startup_commands
 # other debugger options:
-#arm-none-eabi-gdb --tui --nx target/thumbv7em-none-eabi/release/edu-ciaa -x gdb_from_commandline_startup_commands
-ddd --debugger "arm-none-eabi-gdb" --command gdb_from_commandline_startup_commands target/thumbv7em-none-eabi/debug/edu-ciaa
+#arm-none-eabi-gdb --tui --nx target/thumbv7em-none-eabi/release/edu-ciaa -x boards/ciaa/edu-ciaa/gdb_from_commandline_startup_commands
+ddd --debugger "arm-none-eabi-gdb" --command boards/ciaa/edu-ciaa/gdb_from_commandline_startup_commands_ddd target/thumbv7em-none-eabi/debug/edu-ciaa
 
 ## JLink allows us to properly debug line by line!!!!!!!!!!
 ### Start server from /opt/SEGGER/JLink/JLinkGDBServerExe
