45a70f5f794a6a2cbe2db26dc2c589dff7d5c654
Emilio Moretti <emilio.moretti@gmail.com>
2020-06-13 10:11:32 -0300

    Fix ATimer implementation
    It's NOT used anywhere though. I ended up using Timer1 with a much
    better resolution and speed.

--- chips/lpc43xx/src/atimer.rs
+++ chips/lpc43xx/src/atimer.rs
@@ -32,16 +32,22 @@ set_stat: WriteOnly<u32>,
 const ATIMER_BASE: StaticRef<AtimerRegisters> =
     unsafe { StaticRef::new(0x40040000 as *const AtimerRegisters) };
 /*
-DEPRECATED: the atimer pheripheral cannot be used with the Alarm and Time traits.
-You need a counter that goes up, and the alarm timer uses a downcounter
+DEPRECATED: the atimer pheripheral must be used with the Timer trait instead
+of the Alarm trait. I partially implemented this because I ended up using
+Timer1 which is 32 bits and runs at much faster clock.
+ATimer on the other hand is only 16 bit at 1024hz
+*/
+
 pub struct AlarmTimer<'a> {
     registers: StaticRef<AtimerRegisters>,
     callback: OptionalCell<&'a dyn time::AlarmClient>,
+    repeating : mut bool,
 }
 
 pub static mut ATIMER: AlarmTimer<'static> = AlarmTimer {
     registers: ATIMER_BASE,
     callback: OptionalCell::empty(),
+    repeating: false,
 };
 
 impl Controller for AlarmTimer<'a> {
@@ -124,6 +130,10 @@ impl AlarmTimer<'a> {
         self.callback.map(|cb| {
             cb.fired();
         });
+        if !self.repeating {
+            self.disable_registers();
+            self.disable_alarm_irq()
+        }
     }
 }
 
@@ -149,32 +159,84 @@ impl Time for AlarmTimer<'a> {
     }
 }
 
-impl Alarm<'a> for AlarmTimer<'a> {
-    fn set_client(&self, client: &'a dyn time::AlarmClient) {
+/// UNTESTED!!!
+impl Timer<'a> for AlarmTimer<'a> {
+    /// Set the client for interrupt events.
+    fn set_client(&'a self, client: &'a dyn TimerClient) {
         AlarmTimer::set_client(self, client);
     }
-
-    fn set_alarm(&self, tics: u32) {
+    /// Sets a one-shot timer to fire in `interval` clock-tics.
+    ///
+    /// Calling this method will override any existing oneshot or repeating timer.
+    fn oneshot(&self, interval: W){
         let regs: &AtimerRegisters = &*self.registers;
         self.clear();
         regs.preset.set(tics);
+        self.repeating = false;
+        self.enable_alarm_irq();
+        self.enable_registers();
+        
+    }
+    /// Sets repeating timer to fire every `interval` clock-tics.
+    ///
+    /// Calling this method will override any existing oneshot or repeating timer.
+    fn repeat(&self, interval: W){
+        let regs: &AtimerRegisters = &*self.registers;
+        self.clear();
+        regs.preset.set(tics);
+        self.repeating = true;
         self.enable_alarm_irq();
         self.enable_registers();
     }
 
-    fn get_alarm(&self) -> u32 {
-        let regs: &AtimerRegisters = &*self.registers;
+    /// Returns the interval for a repeating timer.
+    ///
+    /// Returns `None` if the timer is disabled or in oneshot mode and `Some(interval)` if it is
+    /// repeating.
+    fn interval(&self) -> Option<W> {
+        if self.is_enabled() && self.is_repeating() {
         regs.preset.get()
+        } else {
+            None
+        }
     }
 
-    fn disable(&self) {
+    /// Returns whether this is a oneshot (rather than repeating) timer.
+    fn is_oneshot(&self) -> bool {
+        self.interval().is_none()
+    }
+
+    /// Returns whether this is a repeating (rather than oneshot) timer.
+    fn is_repeating(&self) -> bool {
+        self.interval().is_some()
+    }
+
+    /// Returns the remaining time in clock tics for a oneshot or repeating timer.
+    ///
+    /// Returns `None` if the timer is disabled.
+    fn time_remaining(&self) -> Option<W>{
+        let regs: &AtimerRegisters = &*self.registers;
+        if self.is_enabled(){
+            Some(self.get_counter())
+        } else {
+            None
+        }
+    }
+
+    /// Returns whether this timer is currently active (has time remaining).
+    fn is_enabled(&self) -> bool {
+        AlarmTimer::is_alarm_enabled(self)
+    }
+
+    /// Cancels an outstanding timer.
+    ///
+    /// The implementation will _always_ cancel the timer.
+    /// delivered.
+    fn cancel(&self) {
         self.disable_alarm_irq();
         self.clear();
         self.disable_registers();
     }
-
-    fn is_enabled(&self) -> bool {
-        AlarmTimer::is_alarm_enabled(self)
-    }
 }
-*/
+
+
